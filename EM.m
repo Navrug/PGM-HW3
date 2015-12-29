809d779a27320850ef404a4c8387f3aad3788720
Gurvan <gurvan.lhostis@gmail.com>
2015-12-29 16:31:06 +0100

    EM, not working yet.

---
+++ EM.m
@@ -0,0 +1,50 @@
+function [ labels, pi, A, mu, sigma ] = EM( data, k, eps )
+%GM2 Summary of this function goes here
+%   Detailed explanation goes here
+
+   [labels, pi, mu, sigma] = GM2(data, k, eps);
+   A = ones(k,k)/6 + diag(ones(k,1))/3;
+
+   [T,d] = size(data);
+
+   L = -1e10;
+   max_it = 100;
+   it = 0;
+   while it < max_it
+      it = it + 1;
+      % E step
+      loga = alphas(data,A,mu,sigma,pi);
+      logb = betas(data,A,mu,sigma);
+      pz = compute_pz(loga, logb);
+      pzz = compute_pzz(loga, logb, data, A, mu, sigma);
+      
+      L_old = L;
+      L = pz(1,:)*pi';
+      for t = 2:T
+         L = L + pz(t,:)*log(mvnpdf(data(t,:),mu,sigma));
+         L = L + sum(sum(A.*reshape(pzz(t-1,:,:),k,k)));
+      end
+     
+      if L - L_old < eps
+         break
+      end
+      
+      % M step
+      A = reshape(sum(pzz, 1),k,k);
+      A = diag(1 ./ sum(A,2))*A;
+      pi = mean(pz,1);
+      mu = zeros(k,d);
+      for j = 1:k
+         mu(j,:) =  + pz(:,j)'*data/sum(pz(:,j))  ; 
+         sigma(:,:,j) = zeros(d);
+         for t = 1:T 
+            sigma(:,:,j) = sigma(:,:,j) + pz(t,j)*(data(t,:)-mu(j,:))'*(data(t,:)-mu(j,:));
+         end
+         sigma(:,:,j) = sigma(:,:,j)/(sum(pz(:,j)));
+      end
+      [~,labels] = max(pz,[],2);
+   end
+   disp(it)
+   
+end
+
---
+++ compute_pzz.m
@@ -0,0 +1,28 @@
+function [ p ] = compute_pzz( loga, logb, data, A, mu, sigma )
+%P Summary of this function goes here
+%   Detailed explanation goes here
+   [~,k] = size(loga);
+   
+   logp = zeros(length(loga)-1, k, k);
+   for t=1:(length(loga)-1)
+      logp(t,:,:) = log(A);
+   end
+   for j = 1:k
+      logp(:,:,j) = logp(:,:,j) + logb(2:length(logb),:);
+      for t=1:(length(loga)-1)
+         logp(t,:,j) = log(mvnpdf(data(t,:),mu,sigma)');
+      end
+   end
+   sz = size(loga);
+   for i = 1:k
+      logp(:,i,:) = logp(:,i,:) + reshape(loga(1:(length(loga)-1),:), sz(1)-1, 1, sz(2));
+   end
+   logsum = logp(:,1,1);
+   for i = 1:k
+      for j = (1+(i==1)):k
+         logsum = logsum + log(1 + exp(logp(:,i,j) - logsum));
+      end
+   end
+   p = exp(bsxfun(@minus,logp, logsum));
+end
+
